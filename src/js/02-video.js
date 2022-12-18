494f30a19268ad7bc287aa81d6799ae06b00781f
Paul Droshniev <108367655+humbubahumbuba@users.noreply.github.com>
2022-12-18 14:44:01 +0200

    add-tasks-hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import _throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Player(iframe);
+
+localStorage.getItem('videoplayer-current-time')
+  ? iframePlayer.setCurrentTime(
+      localStorage.getItem('videoplayer-current-time')
+    )
+  : 0;
+
+iframePlayer.on(
+  'timeupdate',
+  _throttle(function (e) {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      Math.floor(e.seconds).toString()
+    );
+    console.log(
+      'videoplayer-current-time (seconds) >> ',
+      localStorage.getItem('videoplayer-current-time')
+    );
+  }, 1000)
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+import _throttle from 'lodash.throttle';
 
+let feedbackFormState = {
+  email: '',
+  message: '',
+};
+if (!localStorage.getItem('feedback-form-state')) {
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(feedbackFormState)
+  );
+}
+
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', _throttle(onInputChange, 500));
+form.addEventListener('submit', onSubmit);
+
+form.elements.email.required = 'true';
+form.elements.message.required = 'true';
+
+onFormUpdate();
+
+function onFormUpdate() {
+  feedbackFormState = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  form.elements.email.value = feedbackFormState.email;
+  form.elements.message.value = feedbackFormState.message;
+}
+
+function onInputChange() {
+  feedbackFormState.email = form.elements.email.value;
+  feedbackFormState.message = form.elements.message.value;
+
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(feedbackFormState)
+  );
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const submitData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  console.log('submitData :>> ', submitData);
+
+  feedbackFormState.email = '';
+  feedbackFormState.message = '';
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(feedbackFormState)
+  );
+
+  e.currentTarget.reset();
+}
